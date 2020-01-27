04c95c1591bb5f35802d6f1557f1bdf9dd55cb9e
Adam Ahrens <adam.aahrens@gmail.com>
2020-01-26 21:00:18 -0600

    Adding inital empty SwiftUI project

---
+++ RGBullseye/RGBullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullseye
+//
+//  Created by Adam Ahrens on 1/26/20.
+//  Copyright © 2020 Adam Ahrens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
